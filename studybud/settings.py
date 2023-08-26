da6f282411d6d7044faee204f54716cdb2476d1a
cristianferreira13 <criatian1398@gmail.com>
2023-08-26 16:42:03 -0500

    Initial commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for studybud project.
 
@@ -132,7 +133,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-# STATIC_ROOT = 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
